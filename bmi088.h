d08e257aa3de3872f548241254e99503d13e35fd
Fikret Selim Tacettin <ftacettin25@ku.edu.tr>
2026-05-22 21:37:29 +0300

    bmi088 committed

--- bmi088.h
+++ bmi088.h
@@ -2,6 +2,23 @@
 #define BMI088_H
 
 #include <stm32f4xx_hal.h>
+
+#define ACCEL_CS_PORT GPIOC
+#define ACCEL_CS_PIN  GPIO_PIN_5
+#define GYRO_CS_PORT  GPIOC
+#define GYRO_CS_PIN   GPIO_PIN_4
+
+#define BMI088_ACC_CHIP_ID      0x00
+#define BMI088_ACC_DATA_START   0x12
+#define BMI088_ACC_PWR_CONF     0x7C
+#define BMI088_ACC_PWR_CTRL     0x7D
+
+#define BMI088_GYRO_CHIP_ID     0x00
+#define BMI088_GYRO_DATA_START  0x02
+#define BMI088_GYRO_RANGE       0x0F
+#define BMI088_GYRO_BW          0x10
+
+
 uint8_t BMI088_Init(void);
 
 

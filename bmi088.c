13b47c0be4a4628fd8c3982ace008f7eb6161a9e
Fikret Selim Tacettin <ftacettin25@ku.edu.tr>
2026-05-22 21:35:29 +0300

    bmi088 committed

--- bmi088.c
+++ bmi088.c
@@ -3,22 +3,6 @@
 
 extern SPI_HandleTypeDef hspi1;
 
-// PINS
-#define ACCEL_CS_PORT GPIOC
-#define ACCEL_CS_PIN  GPIO_PIN_5
-#define GYRO_CS_PORT  GPIOC
-#define GYRO_CS_PIN   GPIO_PIN_4
-
-// Accelometer registers
-#define BMI088_ACC_CHIP_ID      0x00
-#define BMI088_ACC_DATA_START   0x12
-#define BMI088_ACC_PWR_CONF     0x7C
-#define BMI088_ACC_PWR_CTRL     0x7D
-// Gyro registers
-#define BMI088_GYRO_CHIP_ID     0x00
-#define BMI088_GYRO_DATA_START  0x02
-#define BMI088_GYRO_RANGE       0x0F
-#define BMI088_GYRO_BW          0x10
 
 
 void BMI088_Accel_Write(uint8_t reg, uint8_t data) {
